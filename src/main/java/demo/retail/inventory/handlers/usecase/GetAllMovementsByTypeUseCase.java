96caccc30950d554823dbdbbcf360790d147ee0b
Jose Moyano <josemoyca@gmail.com>
2023-12-20 15:53:57 -0400

    get all movements by retail and wholesale type  use cases, query and router added also record types enum added

---
+++ src/main/java/demo/retail/inventory/handlers/usecase/GetAllMovementsByTypeUseCase.java
@@ -0,0 +1,25 @@
+package demo.retail.inventory.handlers.usecase;
+
+
+import demo.retail.inventory.drivenAdapters.repository.IMovementRepository;
+import demo.retail.inventory.models.Movement;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Function;
+
+@Service
+@Validated
+public class GetAllMovementsByTypeUseCase implements Function<String, Flux<Movement>> {
+    private IMovementRepository repository;
+
+    public GetAllMovementsByTypeUseCase(IMovementRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Flux<Movement> apply(String type) {
+        return repository.findByAllType(type);
+    }
+}
---
+++ src/main/java/demo/retail/inventory/models/RecordTypes.java
@@ -0,0 +1,15 @@
+package demo.retail.inventory.models;
+
+public enum RecordTypes {
+    INVENTORY,
+    INVENTORY_GET,
+    INVENTORY_POST,
+    INVENTORY_POST_BATCH,
+    SALE,
+    RETAIL_SALE,
+    WHOLESALE,
+    PRODUCT,
+    RECORD,
+    MOVEMENT,
+    INTERNAL;
+}
